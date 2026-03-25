3294959c32aaa5de59ad13527e9e5cbdc4cec131
benjaminezequiel <contato.benjaminezequiel@gmail.com>
2026-03-25 22:54:08 +0000

    include vercel analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,16 @@
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: [
-    "@pinia/nuxt",
-    "pinia-plugin-persistedstate/nuxt",
-    "@vercel/analytics",
-  ],
+  modules: ["@pinia/nuxt", "pinia-plugin-persistedstate/nuxt"],
   css: ["leaflet/dist/leaflet.css"],
+  app: {
+    head: {
+      script: [
+        {
+          src: "/_vercel/insights/script.js",
+          defer: true,
+        },
+      ],
+    },
+  },
 });
